07d66c29ff0d0217efdc826091566e43ee046e52
lenovo <ramzeskin@mail.ru>
2025-10-07 00:29:56 +0500

    v2

--- lesson_1.py
+++ lesson_1.py
@@ -13,4 +13,32 @@
 # length = float(input('room length: '))
 # print(f'Square: ', width + length)
 
+# 4.
+# celsius = int(input('Enter degrees in °C: '))
+# fahrenheit = celsius * 9 / 5 + 32
+# print(f'{celsius} °C equals to: {fahrenheit}°F')
+
+# 5.
+# km = float(input('Enter distance in km: '))
+# print(f'{km} km equals to', km * 0.621371, 'miles')
+
+# 6.
+# a = int(input('enter number for a: '))
+# b = int(input('enter number for b: '))
+# print(f'a + b = {a + b}')
+# print(f'a - b = {a - b}')
+# print(f'a x b = {a * b}')
+# print(f'a : b = {a // b}')
+# print(f'a % b = {a % b}')
+# print(f'a ** b = {a ** b}')
+
+# 7.
+# num = int(input('Enter number: '))
+# print(f'Next number:', num + 1 )
+# print(f'Previous number:', num - 1 )
+
+
+
+
+
 
