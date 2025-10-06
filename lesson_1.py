18464909b04f68eb44982047bd9cd470a6e11a5d
lenovo <ramzeskin@mail.ru>
2025-10-06 23:22:41 +0500

    v1

---
+++ lesson_1.py
@@ -0,0 +1,16 @@
+# 1.
+# name = input('Enter your name: ')
+# print(f'Hello {name}!')
+
+# 2.
+# first = int(input('Enter 1st number: '))
+# second = int(input('Enter 2nd number: '))
+# third = int(input('Enter 3rd number: '))
+# print(f'Summ:', first + second + third)
+
+# 3.
+# width = float(input('room width: '))
+# length = float(input('room length: '))
+# print(f'Square: ', width + length)
+
+
