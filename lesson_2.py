24a5b36b144d4ab5b61579f211a6df6373ed1895
Ramil <ramil.yunusov@xb.mail.uz>
2025-10-11 20:55:12 +0500

    v1

---
+++ lesson_2.py
@@ -0,0 +1,33 @@
+# num1 = 15
+# num2 = 10
+# num3 = 20
+# num4 = 15
+#
+# print(num1 > num2)
+# print(num2 > num3)
+# print(num1 == num4)
+
+# str1 = 'ab'
+# str2 = 'abc'
+# str3 = 'abCde'
+#
+# print(str2 > str1)
+# print(str3 > str2)
+# print(str3 == str2)
+
+# Method str
+# word = 'Hello My Friend!'
+# print(word.upper())
+# print(word.lower())
+# print(word.title())
+# print(word.capitalize())
+# print(word.count('e'))
+
+# Conditions
+num = int(input('Enter number: '))
+if num > 10:
+    print(f'Number {num} bigger then 10')
+elif num == 10:
+    print(f'Number {num} equals 10')
+else:
+    print(f'Number {num} less then 10')
