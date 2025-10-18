1af289293f21970fb5c78efccdefdc477258f5c7
Ramil <ramil.yunusov@xb.mail.uz>
2025-10-18 14:26:50 +0500

    v2

--- lesson_2.py
+++ lesson_2.py
@@ -24,12 +24,31 @@
 # print(word.count('e'))
 
 # Conditions
-num = int(input('Enter number: '))
-if num > 10:
-    print(f'Number {num} bigger then 10')
-elif num == 10:
-    print(f'Number {num} equals 10')
-else:
-    print(f'Number {num} less then 10')
+# num = int(input('Enter number: '))
+# if num > 10:
+#     print(f'Number {num} bigger then 10')
+# elif num == 10:
+#     print(f'Number {num} equals 10')
+# else:
+#     print(f'Number {num} less then 10')
 
-h
+# weather = input('Enter your weather: ').lower()
+# if weather == 'sunny' or weather == 'cloudy':
+#     print('You may go walk to the park')
+# elif weather == 'snowy' or weather == 'rainy':
+#     print('You better stay home')
+# else:
+#     print('Good time to have sleep')
+
+# username = input('Enter your login: ').lower()
+# password = input('Enter your password: ')
+#
+# if username and not password:
+#     print('You did not provide password')
+# elif password and not username:
+#     print('You did not provide username')
+#
+# if username == 'ramil' and password == '123' or username == 'yunusov' and password == '123':
+#     print(f'Hello my king {username}')
+# else:
+#     print('login or password not correct')
