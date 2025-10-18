c7eaf657fb47727c442b1a22186431dc0b0197d5
Ramil <ramil.yunusov@xb.mail.uz>
2025-10-18 15:59:31 +0500

    v2

--- lesson_2.py
+++ lesson_2.py
@@ -1,1 +1,12 @@
 # 1.
+age = int(input('Enter your age: '))
+if age <= 18:
+    print('You have to study')
+elif age > 18 and age <= 50:
+    print('You have to work')
+elif age > 50 and age <= 59:
+    print('You are about pension age')
+elif age > 59 and age < 110:
+    print('You are on pension')
+else:
+    print('Incorrect age numbers, please try again')
