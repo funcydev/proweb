2e6b5df7ef92b2ac6a7b72d8a42ccaf65d251b06
Ramil <ramil.yunusov@xb.mail.uz>
2025-11-01 22:02:01 +0500

    v1

---
+++ lesson_3.py
@@ -0,0 +1,39 @@
+# #1.
+# name = input('Enter your name: ')
+# surname = input('Enter your surname: ')
+# age = input('Enter your age: ')
+# lst = [name, surname, age]
+# print(lst)
+
+# #2.
+# lst = input('Enter any numbers separated by comma and space: ')
+# lst2 = [lst]
+# cortage = tuple(lst2)
+# print(f'list: {lst2} \ncortage: {cortage}')
+
+# #3.
+# name = input('Enter 3 different name separated by space: ')
+# sep = name.split(' ')
+# print(sep[0:1])
+# print(sep[1:2])
+# print(sep[2:3])
+
+# #4.
+# letter = input('Enter rate in letter: ').upper()
+# lst = ['A', 'B', 'C', 'D', 'F', 'A+', 'B+', 'C+', 'D+', 'F+', 'A-', 'B-', 'C-', 'D-', 'F-',]
+# if letter in lst:
+#     print('True')
+# else:
+#     print('False')
+
+# #5.
+# lst = input('Enter list of words: ').lower()
+# lst2 = lst.split(' ')
+# if 'python' in lst2:
+#     lst2.remove('python')
+#     print(lst2)
+# else:
+#      lst2.insert(0,'python')
+#      print(lst2)
+
+
