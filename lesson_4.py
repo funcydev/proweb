c433f9020425e4f531bd8a958c32c2d8c9ef1a01
Ramil <ramil.yunusov@xb.mail.uz>
2025-11-06 18:09:41 +0500

    v2

---
+++ lesson_4.py
@@ -0,0 +1,49 @@
+# # 1.
+# lst = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# loreq = []
+# mothen = []
+# for i in lst:
+#     if i <= 5:
+#       loreq.append(i)
+#     else:
+#       if i > 5:
+#         mothen.append(i)
+# print(loreq)
+# print(mothen)
+
+# # 2.
+# plus = input('Enter numbers and "+" operation to see result: ')
+# lst = plus.split(' ')
+# num = []
+# for i in lst:
+#     if i != '+':
+#         num.append(int(i))
+# res = sum(num)
+# print(f'Sum = {res}')
+
+# # 3.
+# lst = input(f'Enter any list of numbers: ')
+# lst2 = lst.split()
+# num = []
+# for i in lst2:
+#     if int(i) > 0:
+#         num.append(int(i))
+# max_number = num[0]
+# for n in num:
+#     if n > max_number:
+#         max_number = n
+# len = (len(num))
+# res = max_number / len
+# print(f'result is: {res}')
+
+
+# 4.
+
+
+
+
+
+
+
+
+
