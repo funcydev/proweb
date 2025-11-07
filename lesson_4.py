04dd27c09279f4bbcc5f6e409e57ef00b2a424a8
Ramil <ramil.yunusov@xb.mail.uz>
2025-11-07 17:40:59 +0500

    v3

--- lesson_4.py
+++ lesson_4.py
@@ -11,6 +11,7 @@
 # print(loreq)
 # print(mothen)
 
+
 # # 2.
 # plus = input('Enter numbers and "+" operation to see result: ')
 # lst = plus.split(' ')
@@ -20,6 +21,7 @@
 #         num.append(int(i))
 # res = sum(num)
 # print(f'Sum = {res}')
+
 
 # # 3.
 # lst = input(f'Enter any list of numbers: ')
@@ -37,11 +39,55 @@
 # print(f'result is: {res}')
 
 
-# 4.
+# # 4.
+# num = input(f'Enter numbers separated by comma ",": ')
+# num2 = num.split(',')
+# x = []
+# for i in num2:
+#         x.insert(0, i)
+# print(x)
 
 
+# # 5.
+# lst = input(f'Enter score: ')
+# score = lst.split()
+#
+# x = []
+# for i in score:
+#     if i == 'A+':
+#        x.append(5.0)
+#     elif i == 'A':
+#         x.append(4.0)
+#     elif i == 'A-':
+#         x.append(3.7)
+#     elif i == 'B+':
+#         x.append(3.3)
+#     elif i == 'B':
+#         x.append(3.0)
+#     elif i == 'B-':
+#         x.append(2.7)
+#     elif i == 'C+':
+#         x.append(2.3)
+#     elif i == 'C':
+#         x.append(2.0)
+#     elif i == 'C-':
+#         x.append(1.7)
+#     elif i == 'D+':
+#         x.append(1.3)
+#     elif i == 'D':
+#         x.append(1.0)
+#     elif i == 'F':
+#         x.append(0)
+# print(f'Total score: {sum(x)}')
+# print(f'Average score: {(sum(x) / len(x)):.2f}')
 
 
+# # 6.
+# num = [1, 2, 3, 4, 5, 6, 7]
+# x = []
+# for i in num:
+#     x.append(i * i)
+# print(x)
 
 
 
