528840f22a2a9602c9d7cc3798ed2df1c5a914f4
Ramil <ramil.yunusov@xb.mail.uz>
2025-11-28 22:17:45 +0500

    v3

--- lesson_6.py
+++ lesson_6.py
@@ -1,147 +1,128 @@
-# #  Slovari dict()
-# # also is type of data - associativniy massiv
 #
-# dict1 = {}          # sozdanie pustogo slovarya
-# dict2 = dict()      # sozdanie pustogo slovarya na osnove classa
+# for num in range(1, 11):
+#     if num % 2 ==0:
+#         continue
+#     print(num)
+
+# words = ["hello", "skip", "word", "python"]
+# for word in words:
+#     if word == "skip":
+#         continue
+#     print(word)
+
+# data = [1, -1, 2, -2, 3, -3, 0]
+# for num in data:
+#     if num <=0:
+#         continue
+#     print(f' Vivod tolko polojitelnih chisel: {num}')
+
+# cnt = 1
+# while cnt <=6:
+#     print(f'Counter equals: {cnt}')
+#     cnt += 1
+
+# user_input = ''
+# while user_input.lower() != 'exit':
+#     user_input = input(f'Enter command to quit: ')
+
+# prod = ['apple', 'eggs', 'banana', 'tomato', 'candy']
+# search = 'banana'
+# found = False
+# index = 0
 #
-# print(dict1)
-# print(dict2)
-#
-# contacts = {
-#     # Key      # value
-#      'mother': '+998998350199',
-#      'Sister': 998998370199,
-#     'brother': '998998410199',
-#           102: 'Police'
-# }
-#
-# print(contacts)
-# # get value by key
-# print(contacts['brother'])
-#
-# # Adding new key with value
-# contacts['soulmate'] = 998998550199
-# print(contacts)
-#
-# # Edit the value
-# contacts[102] = 'musorskaya'
-# print(contacts)
-#
-# # Delete key and value
-# del contacts[102]           # Delete by command
-# print(contacts )
-#
-# contacts.pop('brother')     # Delete by method
-# print(contacts)
-#
+# while index < len(prod) and not found:
+#     if prod[index] == search:
+#         found = True
+#         print(f'Element {search} found on {index} position')
+#     index += 1
+# if not found:
+#     print(f'Element {search} not found')
+
+# while True:
+#     user_iput = input('Enter stop command: ').lower()
+#     if user_iput == 'stop':
+#         break
+# print(f'You have entered {user_iput} command')
+
+# data = []
+# while True:
+#     item = get_new_data()
+#     if not item:
+#         continue
+#     data.append(item)
+#     if len(data) >= 100:
+#         process_data(data)
+#         data.clear()
 
 
-# human = {
-#     'name': 'Ramil',
-#     'lastname': 'Yunusov',
-#     'age': 33,
-#     'phone': '+998998190199',
-#     'merried': True,
-#     'friends': ['Bober', 'Solovey'],
-#     'address': {
-#         'city': 'Tashkent',
-#         'street': 'S.Rahimov',
-#         'home': 8
-#     }
-# }
+# # 1.
+# prod = ['apple', 'banana', 'eggs', 'sausages', 'strawberry', 'onion', 'bread']
+# x = 0
+# found = False
+# search = 'banana'
+# user_input = input(f'Enter command and argument: ').lower()
 #
-# print(human)
-# print(human.keys())     # Method to get keys from dict as a list
+# while x <= len(prod):
+#     if user_input == 'search':
+#         print(f'Enter {} found')
+#     x += 1
+# if not found:
+#     print(f'Element {search} not found')
 #
-# for i in human:
-#     print(i)
+#
+#
+# index = 0
+#
+# while index < len(prod) and not found:
+#     if prod[index] == search:
+#         found = True
+#         print(f'Element {search} found on {index} position')
+#     index += 1
 
-# print(human.values())     # Method to get values from dict as a list
-# for i in human.values():
-#     print(i)
+# for value in range(1, 9):
+#     print(value)
 
-# print(human.items())        # Method to get key and value from dictionary
-# for k, v in human.items():
-#     print(k, v)
-#
-# fixik = {
-#     3: 'dedus',
-#     2: 'simka',
-#     1: 'nolik',
-#     4: 'papus'
-# }
+# squares = [value**2 for value in range(1,6)]
+# print(squares)
 
-# new_dict = {}
-#
-# for col, name in fixik.items():
-#     print(col, name)
-#     new_dict[name] = col
-# print(new_dict)
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[:3])
 
-# dict_num = {}
-# for i in range(1, 6):
-#     dict_num[str(i)] = f'Fixik № {i}'
-# print(dict_num)
-#
-# dict_num2 = {str(i): f'Fixik № {i}' for i in range(1, 6)}
-# print(dict_num2)
-#
-# new_dict = {name: col for col, name in fixik.items()}
-# print(new_dict)
+# lst = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+# print('The first three items in the list are:')
+# print(lst[:3])
 
-# Vlojennost
-# Dictionary of the dictionary
-# Dictionary with list in values
+# lst = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+# print('Three items from the middle of the list are:')
+# print(lst[1:4])
 
-# students = [
-#     # 1 student
-#     {
-#              'name': 'Ramil',
-#         'last_name': 'Yunusov',
-#               'age':  39,
-#           'friends': ['Lola', 'Pola', 'Sola'],
-#             'marks': {
-#                    'math': 5,
-#                 'history': 4,
-#                   'chemy': 3
-#             }
+# lst = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+# print('The last three items in the list are:')
+# print(lst[-3:])
 #
-#     },
-#     # 2 student
-#     {
-#              'name': 'Petr',
-#         'last_name': 'Petrenko',
-#               'age':  22,
-#           'friends': ['Lola', 'Pola', 'Sola'],
-#             'marks': {
-#                    'math': 2,
-#                 'history': 2,
-#                   'chemy': 3
-#             }
-#     },
-#     # 3 student
-#     {
-#              'name': 'Bobr',
-#         'last_name': 'Bobrenko',
-#               'age':  32,
-#           'friends': ['Lola', 'Pola', 'Sola'],
-#             'marks': {
-#                    'math': 4,
-#                 'history': 3,
-#                   'chemy': 5
-#             }
-#     }
-# ]
-#
-# print(students)
-#
-# total = 0
-# for stud in students:
-#     # total += stud['marks']['math']
-#     # total += stud['marks']['history']
-#     # total += stud['marks']['chemy']
-#     total += sum(stud['marks'].values())
-# print(total)
-#
-# average = total / len(students) / len(students[0]['marks'])
-# print(round(average, 1))
+# my_lst = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+# friend_lst = my_lst[:]
+# my_lst.append('Americano')
+# friend_lst.append('Coffe')
+# print(my_lst)
+# print(friend_lst)
+
+
+prod = ['apple', 'banana', 'eggs', 'sausages', 'strawberry', 'onion', 'bread']
+
+user_input = input(f'Enter command and argument: ').lower()
+# if len(user_input.split(' ')) == 2:
+#     command, product = user_input.split(' ')
+#     if command == 'search' and product in prod:
+#         print(f'product {product} in the list')
+#     else:
+#         print(f'product {product} NOT in the list')
+if len(user_input.split(' ')) == 2:
+    command, product = user_input.split(' ')
+    if command == 'clear' and product in prod:
+        print(product)
+        prod.clear(prod)
+        print(prod)
+
+
+
