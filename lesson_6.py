a56b6451061908c0c3bd0cab5a51da758e912287
Ramil <ramil.yunusov@xb.mail.uz>
2025-12-02 20:16:33 +0500

    v4

--- lesson_6.py
+++ lesson_6.py
@@ -3,6 +3,7 @@
 #     if num % 2 ==0:
 #         continue
 #     print(num)
+from pyparsing import White
 
 # words = ["hello", "skip", "word", "python"]
 # for word in words:
@@ -107,22 +108,62 @@
 # print(my_lst)
 # print(friend_lst)
 
+# #1.
+# prod = ['apple', 'banana', 'eggs', 'sausages', 'strawberry', 'onion', 'bread']
+# while True:
+#     user_input = input(f'Enter command and argument: ').lower()
+#     if len(user_input.split(' ')) == 2:
+#         command, product = user_input.split(' ')
+#         if command == 'search' and product in prod:
+#             print(f'product "{product}" in the list')
+#         elif command == 'search' and product not in prod:
+#             print(f'product "{product}" NOT in the list')
+#         elif command == 'clear' and product in prod:
+#             prod.remove(product)
+#             print(f'Product "{product}" removed from list')
+#             print(prod)
+#         else:
+#             print('Command or an argument not correct')
+#     if user_input == 'stop':
+#         break
 
-prod = ['apple', 'banana', 'eggs', 'sausages', 'strawberry', 'onion', 'bread']
+# #2.
+# n = int(input(f'Enter max number: '))
+# for i in range(1, n + 1):
+#     for x in range(i):
+#         print(i, end=' ')
+#     print()
 
-user_input = input(f'Enter command and argument: ').lower()
-# if len(user_input.split(' ')) == 2:
-#     command, product = user_input.split(' ')
-#     if command == 'search' and product in prod:
-#         print(f'product {product} in the list')
+# #3.
+# i = input('Enter two positive numbers via space: ')
+# y = []
+# x = 1
+# if len(i.split(' ')) == 2:
+#     n, m = i.split(' ')
+#     n = int(n)
+#     m = int(m)
+#     while x <= m:
+#         if n % x == 0 and m % x == 0:
+#             y.append(x)
+#             x += 1
+#         else:
+#             x += 1
+# print(max(y))
+
+# # 4.
+# summ = int(input('Enter summ: '))
+# year = int(input('Enter annual period: '))
+# percent = 10
+# annual = (summ * (year * percent) / 100 + summ)
+# print(f'Annual interest rate is 10% \nAfter {year} year(s) you get {annual} sum')
+
+# #5.
+# import random
+# n = random.randint(1, 100)
+# while True:
+#     user_input = int(input('Guess the number: '))
+#     if user_input == n:
+#         print('You won!')
+#         break
 #     else:
-#         print(f'product {product} NOT in the list')
-if len(user_input.split(' ')) == 2:
-    command, product = user_input.split(' ')
-    if command == 'clear' and product in prod:
-        print(product)
-        prod.clear(prod)
-        print(prod)
-
-
-
+#         print(f'Try one more time \nRandom number was: {n}')
